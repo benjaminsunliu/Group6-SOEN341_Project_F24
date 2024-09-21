c43a5548d6350ced4a463eaa6bf5ae690885c264
oldgrandma101 <adphelps8@gmail.com>
2024-09-21 17:52:19 -0400

    added login, home, nav bar, footer, routing(page navigation)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
-import './App.css';
+import "./App.css";
+import Footer from "./layout/Footer";
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is the beginning of our website</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <body class="d-flex h-100 text-center">
+          {" "}
+          {/* can change text-bg-dark to text-bg-light */}
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+          </Routes>
+        </body>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
