9dd19b95203f1ba7ed36d7f613075b7863b923c0
Jordan Yeh <jordanyeh2003@gmail.com>
2024-09-29 13:05:54 -0400

    Delete client directory
    Accidentally added it onto main instead of login_UI branch

--- client/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = (props) => {
-  return (
-    <button className={props.buttonColor} onClick={props.onClick}>
-      {props.buttonText}
-    </button>
-  );
-};
-
-export default Button;
