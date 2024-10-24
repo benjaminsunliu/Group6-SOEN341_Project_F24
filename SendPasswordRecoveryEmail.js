772c1c36592c9740bada99999c57c75889a01a52
Mehrad25Software <mehradmostaan.software@gmail.com>
2024-10-24 14:04:54 -0400

    Update SendPasswordRecoveryEmail.js

--- SendPasswordRecoveryEmail.js
+++ SendPasswordRecoveryEmail.js
@@ -66,7 +66,7 @@ async function SendPasswordRecoveryEmail(email) {
 
             text: `You are receiving this because you or someone else have requested the reset of the password for your account.\n\n
             Please click on the following link, or paste this into your browser to complete the process:\n\n
-            http://yourdomain.com/reset/${token}\n\n
+            http://domain.com/reset/${token}\n\n
             If you did not request this, please ignore this email and your password will remain unchanged.\n`,
         };
 
