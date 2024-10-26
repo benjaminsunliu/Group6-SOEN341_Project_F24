72d7227b3f4090c205e53df62daeb722b7858b08
oldgrandma101 <115679097+oldgrandma101@users.noreply.github.com>
2024-10-26 16:23:15 -0400

    Revert "Login UI"

--- client/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = (props) => {
-  return (
-    <button className={props.buttonColor} onClick={props.onClick} disabled={props.disabled}>
-      {props.buttonText}
-    </button>
-  );
-};
-
-export default Button;
