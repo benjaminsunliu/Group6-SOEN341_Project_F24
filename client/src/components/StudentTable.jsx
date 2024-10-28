7261544663cbaad9d86e81d38cd8405a8ae6941f
oldgrandma101 <115679097+oldgrandma101@users.noreply.github.com>
2024-10-28 00:19:03 -0400

    Update StudentTable.jsx
    more backups

--- client/src/components/StudentTable.jsx
+++ client/src/components/StudentTable.jsx
@@ -1,33 +1,28 @@
 const StudentTable = ({ tableContents }) => {
-    const headers = tableContents.headers;
-    const contents = tableContents.contents;
-  
-    return (
-      <table className="table table-striped" style={{ textAlign: "left" }}>
-        <thead>
-          <tr>
-            {headers.map((header) => (
-              <th key={header.id}>{header.title}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {contents.map((content) => (
-            <tr key={content.studentID}>
-              {headers.map((header) => (
-                <td key={header.id}>
-                  {/* Check if the header is for members and join them with commas */}
-                  {Array.isArray(content[header.id])
-                    ? content[header.id].join(", ")
-                    : content[header.id]}
-                </td>
+
+const headers = tableContents.headers;
+const contents = tableContents.contents;
+
+  return (
+      <table className="table table-striped" style={{textAlign: "left"}}>
+          <thead>
+              <tr>
+                  {headers.map(header => (
+                      <th key={header.id}>{header.title}</th>
+                  ))}
+              </tr>
+          </thead>
+          <tbody>
+              {contents.map(content => (
+                  <tr key={content.studentID}>
+                      {headers.map(header => (
+                          <td key={header.id}>{content[header.id]}</td>
+                      ))}
+                  </tr>
               ))}
-            </tr>
-          ))}
-        </tbody>
+          </tbody>
       </table>
-    );
-  };
-  
+  );
+}
+
   export default StudentTable;
-  
