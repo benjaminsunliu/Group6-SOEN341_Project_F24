856c0aa63159bb9f69eb728e345f1184fd599004
MustafaHunter <142630575+MustafaHunter@users.noreply.github.com>
2024-11-13 01:48:39 -0500

    Update jest.config.js

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-    transform: {
-      '^.+\\.(js|jsx)$': 'babel-jest',
-    },
-    transformIgnorePatterns: [
-      '/node_modules/(?!axios|js-cookie|jwt-decode)',
-    ],
-    testEnvironment: 'jsdom', // Set jsdom environment
-  };
-  
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest', // Use babel-jest to transform JS and JSX files
+  },
+  // Make sure certain modules are transformed, even though they're in node_modules
+  transformIgnorePatterns: [
+    '/node_modules/(?!axios|js-cookie|jwt-decode)', // Transform these specific node_modules
+  ],
+  testEnvironment: 'jsdom', // Set the environment to jsdom (for simulating browser)
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy', // Mock CSS imports using identity-obj-proxy
+  },
+ 
+};
