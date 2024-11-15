91ee086f7ade7391c898245331df9ec1b14691c0
Mehrad25Software <mehradmostaan.software@gmail.com>
2024-11-14 21:12:12 -0500

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,21 +8,30 @@ import CreateAccount from "./pages/CreateAccount";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InstructorDashboard from "./pages/InstructorDashboard";
 import StudentDashboard from "./pages/StudentDashboard";
+import Button from "./components/Button";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <div className="content">
+        <body>
+          {" "}
+          {/* can change text-bg-dark to text-bg-light */}
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/create-account" element={<CreateAccount />} />
-            <Route path="/instructor-dashboard" element={<InstructorDashboard />} />
-            <Route path="/student-dashboard" element={<StudentDashboard />} />
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/create-account" element={<CreateAccount />}></Route>
+            <Route
+              path="/instructor-dashboard"
+              element={<InstructorDashboard />}
+            ></Route>
+            <Route
+              path="/student-dashboard"
+              element={<StudentDashboard />}
+            ></Route>
           </Routes>
-        </div>
+        </body>
         <Footer />
       </div>
     </Router>
