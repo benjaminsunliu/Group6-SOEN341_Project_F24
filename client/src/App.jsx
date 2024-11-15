409948342325c0e2a58f11ebc18cb1047ba5572f
Mehrad25Software <mehradmostaan.software@gmail.com>
2024-11-14 20:05:59 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import "./App.css";
 import Footer from "./layout/Footer";
 import Navbar from "./layout/Navbar";
@@ -8,30 +9,21 @@ import CreateAccount from "./pages/CreateAccount";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InstructorDashboard from "./pages/InstructorDashboard";
 import StudentDashboard from "./pages/StudentDashboard";
-import Button from "./components/Button";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <body>
-          {" "}
-          {/* can change text-bg-dark to text-bg-light */}
+        <div className="content">
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/create-account" element={<CreateAccount />}></Route>
-            <Route
-              path="/instructor-dashboard"
-              element={<InstructorDashboard />}
-            ></Route>
-            <Route
-              path="/student-dashboard"
-              element={<StudentDashboard />}
-            ></Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/create-account" element={<CreateAccount />} />
+            <Route path="/instructor-dashboard" element={<InstructorDashboard />} />
+            <Route path="/student-dashboard" element={<StudentDashboard />} />
           </Routes>
-        </body>
+        </div>
         <Footer />
       </div>
     </Router>
