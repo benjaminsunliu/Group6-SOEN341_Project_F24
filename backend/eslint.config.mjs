1969754a522401f883842abbb779a18ea93e2590
Benjamin Liu <liubenjamin8@gmail.com>
2024-11-28 01:15:22 -0500

    Static code analysis w/ ESLint

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
