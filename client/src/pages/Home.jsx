514d3f71e147c88b418f73b25acb0ed65ac656c3
Benjamin Liu <liubenjamin8@gmail.com>
2024-11-28 01:51:18 -0500

    Fixed Home page styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,13 @@ const Home = (props) => {
   return (
     <>
       <div class="body" className="body">
+        <h1 className="home">Welcome to RateMyPeers! </h1>
+        <img src="./images/home.png" alt="home" width="500"/>
+        <br/>
+        <p class="home" className="home">
+        This is a platform where students can rate their peers based on their performance in group projects. This platform is designed to help students find the best group members for their projects.
+        </p>
         <main className="body" class="body">
-          <h1 className="home">Welcome to RateMyPeers! </h1>
-          <img src="./images/home.png" alt="home" width="500"/>
-          <p class="home" className="home">
-          This is a platform where students can rate their peers based on their performance in group projects. This platform is designed to help students find the best group members for their projects.
-          </p>
         </main>
       </div>
     </>
